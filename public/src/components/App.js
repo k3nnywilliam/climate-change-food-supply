f5db2538504f3f6c76cb8787c28b26a1e3d925c5
k3nnywilliam <k3nnywilliam@gmail.com>
2021-10-21 21:30:40 +0800

    Add react fragments

--- public/src/components/App.js
+++ public/src/components/App.js
@@ -1,17 +1,26 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import React, { Component, Fragment } from 'react';
+import './App.css';
+//import { BrowserRouter as Router, Route } from 'react-router-dom';
 import BarChart from './BarChart.js';
+import LineChart from './LineChart.js';
 
 class App extends Component {
     render() {
-        return (
-        /*     <Router>
+        return [
+            <Fragment>
+                <div><img src="images/wheat.jpg" /></div>
+                <p>Photo by <a
+                    href="https://unsplash.com/@erwanhesry?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Erwan
+                    Hesry</a> on <a
+                        href="https://unsplash.com/s/photos/wheat?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+
+                </p>
+                <div id="title">Maize Production</div>
                 <div>
-                    <Route component={BarChart} />
+                    <BarChart />
                 </div>
-            </Router> */
-            <div>HELLO</div>
-        );
+            </Fragment>
+        ];
     }
 }
 
