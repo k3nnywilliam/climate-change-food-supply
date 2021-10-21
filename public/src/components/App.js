f3d26536388446597ae5d0258714dc909dbfbb09
k3nnywilliam <k3nnywilliam@gmail.com>
2021-10-21 17:22:22 +0800

    Refactor React components

---
+++ public/src/components/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import BarChart from './BarChart.js';
+
+class App extends Component {
+    render() {
+        return (
+        /*     <Router>
+                <div>
+                    <Route component={BarChart} />
+                </div>
+            </Router> */
+            <div>HELLO</div>
+        );
+    }
+}
+
+export default App;
