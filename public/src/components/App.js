cfe8ea0e133e61ef8fa2bd0ca7ff93d7ae33ed48
k3nnywilliam <k3nnywilliam@gmail.com>
2021-10-22 14:33:40 +0800

    Add multiple elements of barchart, supertitles, data columns

--- public/src/components/App.js
+++ public/src/components/App.js
@@ -1,11 +1,20 @@
 import React, { Component, Fragment } from 'react';
 import './App.css';
-//import { BrowserRouter as Router, Route } from 'react-router-dom';
 import BarChart from './BarChart.js';
-import LineChart from './LineChart.js';
 
 class App extends Component {
     render() {
+        let maize_data = "data/maize_top_ten.csv";
+        let rice_data = "data/rice_top_ten.csv";
+        let wheat_data = "data/wheat_top_ten.csv";
+        let maize_col  = "MZ_2000";
+        let rice_col  = "RI_2000";
+        let wheat_col  = "WH_2000";
+
+        let maize_title = "Top 10 countries of maize production average 2000 to 2006 in t (FAO)";
+        let rice_title = "Top 10 countries of rice production average 2000 to 2006 in t (FAO)";
+        let wheat_title = "Top 10 countries of wheat production average 2000 to 2006 in t (FAO)";
+
         return [
             <Fragment>
                 <div><img src="images/wheat.jpg" /></div>
@@ -16,9 +25,11 @@ class App extends Component {
 
                 </p>
                 <div id="title">Maize Production</div>
-                <div>
-                    <BarChart />
-                </div>
+               
+                 <BarChart filename={maize_data} datacol={maize_col} suptitle={maize_title} />
+                 <BarChart filename={rice_data} datacol={rice_col} suptitle={rice_title} />
+                 <BarChart filename={wheat_data} datacol={wheat_col} suptitle={wheat_title} />
+               
             </Fragment>
         ];
     }
