8f7921b6697d956e97e450b6d396be13b49857f9
Kenny Nyallau <k3nnywilliam@gmail.com>
2021-10-26 16:56:26 +0800

    Add all data variables into a single object

---
+++ public/src/components/DataVariables.js
@@ -0,0 +1,13 @@
+const data = {
+    maize_data: "src/csv_files/maize_top_ten.csv",
+    rice_data: "src/csv_files/rice_top_ten.csv",
+    wheat_data: "src/csv_files/wheat_top_ten.csv",
+    maize_col: "MZ_2000",
+    rice_col: "RI_2000",
+    wheat_col: "WH_2000",
+    maize_title: "Top 10 countries of maize production average 2000 to 2006 in t (FAO)",
+    rice_title: "Top 10 countries of rice production average 2000 to 2006 in t (FAO)",
+    wheat_title: "Top 10 countries of wheat production average 2000 to 2006 in t (FAO)"
+}
+
+export {data};
