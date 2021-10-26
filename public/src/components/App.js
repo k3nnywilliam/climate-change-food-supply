196f2c78e7036129e41296b1397dccd8125b1669
Kenny Nyallau <k3nnywilliam@gmail.com>
2021-10-26 16:57:01 +0800

    Add all data variables into a single object

--- public/src/components/App.js
+++ public/src/components/App.js
@@ -1,23 +1,13 @@
 import React, { Component, Fragment } from 'react';
 import './App.css';
 import BarChart from './BarChart.js';
+import {data} from './DataVariables.js';
 
 class App extends Component {
     render() {
-        let maize_data = "src/csv_files/maize_top_ten.csv";
-        let rice_data = "src/csv_files/rice_top_ten.csv";
-        let wheat_data = "src/csv_files/wheat_top_ten.csv";
-        let maize_col  = "MZ_2000";
-        let rice_col  = "RI_2000";
-        let wheat_col  = "WH_2000";
-
         const width = 1200;
         const height = 600;
-
-        let maize_title = "Top 10 countries of maize production average 2000 to 2006 in t (FAO)";
-        let rice_title = "Top 10 countries of rice production average 2000 to 2006 in t (FAO)";
-        let wheat_title = "Top 10 countries of wheat production average 2000 to 2006 in t (FAO)";
-
+        
         return [
             <Fragment>
                 <div><img src="images/wheat.jpg" /></div>
@@ -29,9 +19,9 @@ class App extends Component {
                 </p>
                 <div id="title">Maize Production</div>
                
-                 <BarChart filename={maize_data} datacol={maize_col} suptitle={maize_title} width={width} height = {height} />
-                 <BarChart filename={rice_data} datacol={rice_col} suptitle={rice_title} width={width} height={height} />
-                 <BarChart filename={wheat_data} datacol={wheat_col} suptitle={wheat_title} width={width} height={height} />
+                 <BarChart filename={data.maize_data} datacol={data.maize_col} suptitle={data.maize_title} width={width} height = {height} />
+                 <BarChart filename={data.rice_data} datacol={data.rice_col} suptitle={data.rice_title} width={width} height={height} />
+                 <BarChart filename={data.wheat_data} datacol={data.wheat_col} suptitle={data.wheat_title} width={width} height={height} />
                
             </Fragment>
         ];
