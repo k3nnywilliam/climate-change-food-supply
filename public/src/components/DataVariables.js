7e91f76d1b21e26a56485085e17feac1cc8eb2b0
Kenny Nyallau <k3nnywilliam@gmail.com>
2021-10-27 10:47:30 +0800

    Add comment to dataframe column variable

--- public/src/components/DataVariables.js
+++ public/src/components/DataVariables.js
@@ -2,7 +2,7 @@ const data = {
     maize_data: "src/csv_files/maize_top_ten.csv",
     rice_data: "src/csv_files/rice_top_ten.csv",
     wheat_data: "src/csv_files/wheat_top_ten.csv",
-    maize_col: "MZ_2000",
+    maize_col: "MZ_2000", //name of the column
     rice_col: "RI_2000",
     wheat_col: "WH_2000",
     maize_title: "Top 10 countries of maize production average 2000 to 2006 in t (FAO)",
