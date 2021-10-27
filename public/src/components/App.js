b05974ee15ec488b5a7770ad2dcd2dcb2b4edf14
Kenny Nyallau <k3nnywilliam@gmail.com>
2021-10-27 15:54:19 +0800

    Modify csv and image file reference to be imported in its relative folder

--- public/src/components/App.js
+++ public/src/components/App.js
@@ -1,7 +1,11 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component, Fragment, lazy } from 'react';
 import './App.css';
 import BarChart from './BarChart.js';
 import {data} from './DataVariables.js';
+import img from './images/wheat.jpg';
+import maize_data from "./csv_files/maize_top_ten.csv";
+import rice_data from "./csv_files/rice_top_ten.csv";
+import wheat_data from "./csv_files/wheat_top_ten.csv";
 
 class App extends Component {
     render() {
@@ -10,7 +14,7 @@ class App extends Component {
         
         return [
             <Fragment>
-                <div><img src="images/wheat.jpg" /></div>
+                <div><img src={img} /></div>
                 <p>Photo by <a
                     href="https://unsplash.com/@erwanhesry?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Erwan
                     Hesry</a> on <a
@@ -19,9 +23,9 @@ class App extends Component {
                 </p>
                 <div id="title">Maize Production</div>
                
-                 <BarChart filename={data.maize_data} datacol={data.maize_col} suptitle={data.maize_title} width={width} height = {height} />
-                 <BarChart filename={data.rice_data} datacol={data.rice_col} suptitle={data.rice_title} width={width} height={height} />
-                 <BarChart filename={data.wheat_data} datacol={data.wheat_col} suptitle={data.wheat_title} width={width} height={height} />
+                 <BarChart filename={maize_data} datacol={data.maize_col} suptitle={data.maize_title} width={width} height = {height} />
+                 <BarChart filename={rice_data} datacol={data.rice_col} suptitle={data.rice_title} width={width} height={height} />
+                 <BarChart filename={wheat_data} datacol={data.wheat_col} suptitle={data.wheat_title} width={width} height={height} />
                
             </Fragment>
         ];
